6a035db430a3a62d42b7ecd1049b4172dd13a145
VirginiaPoglonig <virginiapoglonig@gmail.com>
2021-07-10 18:47:16 +0200

    js practica

---
+++ Virginia-P/javascript_ejercicios/js_ejercicios_jueves8.js
@@ -0,0 +1,73 @@
+/* 1) SUMA_RESTA_MULTIPLICACIÓN_DIVISISION */
+
+/*let varNumero1= 5;
+let varNumero2= 20;
+
+console.log (varNumero1+varNumero2);
+console.log (varNumero1-varNumero2);
+console.log (varNumero1*varNumero1);
+console.log (varNumero2/varNumero1);
+
+/* 2) CONCATENAR_STRINGS_ */
+
+/*let name= 'juan';
+let surname= 'fernandez';
+let fullname= name + surname;
+console.log (fullname);
+console.log (fullname.length);*/
+
+/*3) GRADOS_CELSIUS*/
+
+/*let degreesC= 20;
+let degreesF= ((degreesC * 9/5) + 32);
+console.log (degreesC);
+console.log (degreesF);*/
+
+
+/* 4) TRUE_FALCE */
+
+/*let age= 50;
+let allowedAge = (age >=10 && age<=100);
+console.log (allowedAge);*/
+
+/*5)  GRADOS_CELCIUS*/
+
+/*let degreesC= 40;
+let degreesF= ((degreesC * 9/5) + 32);
+let tempF= (degreesF > 87);
+console.log (tempF);*/
+
+/* 6) PAR_O_IMPAR */
+
+/*let number=2;
+let pair= (number%2 ==0);
+console.log (pair);*/
+
+/* 7) NUMERO_DE_HIJOS */
+
+/*let numOfChildren= 9
+let bigFamily= (numOfChildren > 4);
+console.log (bigFamily);*/
+
+/* 8) OBESIDAD_DE_CLASE_DOS */
+
+/*let height= 1.60;
+let weight= 95;
+let iMG= (weight / (height**2));
+let iMG2= (35 < iMG  || iMG >39.9);
+console.log (iMG2);*/
+
+/* 9) CESTA_DE_LAS_COMPRAS */
+
+/*let balance= 100;
+let tomatoes= 5;
+let apples= 10;
+let milk= 8;
+let fish= 6;
+let suffBalance= ( balance >= (tomatoes+apples+milk+fish));
+console.log (suffBalance);*/
+
+/* 10) IRPF */
+
+/* 11) BECA */
+
