7fe278ea747de500ea1551805e7be3a2fdadccf6
PalomaReb <paloma.rebolleda@gmail.com>
2021-07-11 19:16:26 +0200

    excercises from first day with javascript, although I need to finish the last 2

--- Paloma/javascript/start-practice.js
+++ Paloma/javascript/start-practice.js
@@ -1,69 +1,76 @@
-// // let numFirst = 23;
-// // let numSecond = 092384;
+//ejercicio 1
+let numFirst = 23;
+let numSecond = 092384;
+console.log (numSecond + numSecond);
+console.log (numSecond - numSecond);
+console.log (numSecond * numSecond);
+console.log (numSecond / numSecond);
+console.log (numSecond % numSecond);
 
-// // console.log (numSecond + numSecond);
-// // console.log (numSecond - numSecond);
-// // console.log (numSecond * numSecond);
-// // console.log (numSecond / numSecond);
-// // console.log (numSecond % numSecond);
+//ejercicio 2
+let stringFirst ='pepinos deliciosos ';
+let stringSecond = 'aguacates maravillosos';
 
-// // let stringFirst ='pepinos deliciosos ';
-// // let stringSecond = 'aguacates maravillosos';
+console.log (stringFirst + stringSecond);
 
-// // console.log (stringFirst + stringSecond);
+//ejercicio 3
+console.log ((stringFirst + stringSecond).length);
 
-// // console.log ((stringFirst + stringSecond).length);
+//ejercicio 4
+let numCelcius = 31;
+let boolCel = (numCelcius * 9/5) + 32 > 87;
+let numfarenheit = (numCelcius * 9/5) + 32;
 
-// // let numCelcius = 31;
-// // let boolCel = (numCelcius * 9/5) + 32 > 87;
-// // let numfarenheit = (numCelcius * 9/5) + 32;
+let numCel =31;
+let boolCel = ((numCel * 9/5) +32) >87;
+console.log ('is' +numCel + 'higher than 87F' + boolCel);
 
-// // let numCel =31;
-// // let boolCel = ((numCel * 9/5) +32) >87;
-// // console.log ('is' +numCel + 'higher than 87F' + boolCel);
+console.log(numCelcius + 'ºC son ' + numfarenheit + 'ºF');
 
-// // console.log(numCelcius + 'ºC son ' + numfarenheit + 'ºF');
+//ejercicio 5
+console.log ((numFirst >= 10) && (numFirst <= 100));
 
-// // console.log ((numFirst >= 10) && (numFirst <= 100));
+//ejercicio 6
+let numHijos = 1
 
-// let numHijos = 1
+console.log ('is a family of ' + numHijos + ' a numerous family? ' + (numHijos >= 3 && numHijos <= 5));
 
-// console.log ('is a family of ' + numHijos + ' a numerous family? ' + (numHijos >= 3 && numHijos <= 5));
+//ejercicio 7
+const weight = 90;
+const height = 1.76;
+let BMI = weight/(height**2);
+console.log ('Type 2 obesity: ' + (BMI >= 30 && BMI <= 35));
 
-// // const weight = 90;
-// // const height = 1.76;
-// // let BMI = weight/(height**2);
-// //  console.log ('Type 2 obesity: ' + (BMI >= 30 && BMI <= 35));
+//ejercicio 8
+let oranges = 6;
+let water = 2;
+let minceMeat =9;
+let tomatoes = 12;
+let sum = oranges + water + minceMeat + tomatoes;
+let balance =45;
 
+console.log ('Can I buy oranges water minced meat tomatoes with my bank balace? ' + (balance >= sum));
 
-// // let oranges = 6;
-// // let water = 2;
-// // let minceMeat =9;
-// // let tomatoes = 12;
-// // let sum = oranges + water + minceMeat + tomatoes;
-// // let balance =45;
+// need to finish this exercise as well as the one after it. 
+let BrSal = 24000;
+let numPays = 12;
+let Segsoc = .0635;
+let IRPF1 = .19;
+let IRPF2 = .24;
+let IRPF3 =.30;
+let IRPF4 = .37;
+let IRPF5 = .45;
 
-// // console.log ('Can I buy oranges water minced meat tomatoes with my bank balace? ' + (balance >= sum));
+console.log('Salario bruto anual: ' + BrSal + ' €');
 
-// let BrSal = 24000;
-// let numPays = 12;
-// let Segsoc = .0635;
-// let IRPF1 = .19;
-// let IRPF2 = .24;
-// let IRPF3 =.30;
-// let IRPF4 = .37;
-// let IRPF5 = .45;
+let BrSalmonth = BrSal/numPays;
+let quoteSS = BrSalmonth*Segsoc;
+let tramo1 = BrSalmonth*IRPF1;
+//let tramo1 = (BrSal >= 0 && BrSal <= 12450) 
 
-// console.log('Salario bruto anual: ' + BrSal + ' €');
-
-// let BrSalmonth = BrSal/numPays;
-// let quoteSS = BrSalmonth*Segsoc;
-// let tramo1 = BrSalmonth*IRPF1;
-// //let tramo1 = (BrSal >= 0 && BrSal <= 12450) 
-
-// console.log ('Monthly social security quote: ' + BrSalmonth*Segsoc + ' €');
-// console.log ('IRPF mensual del primer tramo: ' + (BrSal*IRPF1)/numPays + ' €');
-// console.log ('Salario neto mensual: ' + (BrSalmonth-quoteSS-tramo1) + ' €' );
+console.log ('Monthly social security quote: ' + BrSalmonth*Segsoc + ' €');
+console.log ('IRPF mensual del primer tramo: ' + (BrSal*IRPF1)/numPays + ' €');
+console.log ('Salario neto mensual: ' + (BrSalmonth-quoteSS-tramo1) + ' €' );
 
 
 
