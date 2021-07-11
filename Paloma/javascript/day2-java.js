bffeede7c2f26f9add8face1b6f064f7d83141de
PalomaReb <paloma.rebolleda@gmail.com>
2021-07-11 19:02:41 +0200

    Javascript day 2 excercises completed.

--- Paloma/javascript/day2-java.js
+++ Paloma/javascript/day2-java.js
@@ -14,58 +14,60 @@
 // // // * * * 
 // // // * *
 // // // *
+//Ejercicio que calcule el valor de la sucesión de fibonacci de una variable. Implementar la solución con un while (Hard)
 
 // // // // //ejercicio 1
-// let num1= 54;
-// let num2= 54;
 
-// if (num1 > num2) {
-//     console.log (num1);
-// }
-// else if (num1 === num2) {
-//     console.log ('Same number, sorry loser');
-// }
-// else {
-//     console.log (num2);
-// }
+let num1= 54;
+let num2= 54;
+
+if (num1 > num2) {
+    console.log (num1);
+}
+else if (num1 === num2) {
+    console.log ('Same number, sorry loser');
+}
+else {
+    console.log (num2);
+}
 
 // // // // //ejercicio 2
-// let numfor =6;
 
-// for (i=1; i<=numfor; i++){
-//     if (i%2===0){
-//         console.log (i + ' even');
-//     }
-//     else {
-//         console.log (i + ' odd');
-//     }
-// // } 
+let numfor =6;
+for (let i=1; i<=numfor; i++){
+    if (i%2===0){
+        console.log (i + ' even');
+    }
+    else {
+        console.log (i + ' odd');
+    }
+} 
 // // // // //ejercicio3
 
-// let numEx3 =100;
-// for( i=0; i<=numEx3; i++) {
-//     console.log (i);
-//     if ( i%3===0) {
-//         console.log ('Fizz');
-//     }
-// if ( i%5===0) {
-//     console.log ('Buzz');
-// }
-//  if (i%3===0 && i%5===0) {
-//      console.log ('BuzzFizz');
-//  }
-// }
+let numEx3 =100;
+for( let i=0; i<=numEx3; i++) {
+    console.log (i);
+    if ( i%3===0) {
+        console.log ('Fizz');
+    }
+if ( i%5===0) {
+    console.log ('Buzz');
+}
+ if (i%3===0 && i%5===0) {
+     console.log ('BuzzFizz');
+ }
+}
 
-// for (let i=1; i<100; i++) 
-// {
-// let output ='';
-// if ( i%3===0){
-// }
-// output +='fizz';
-// if ( i%5===0){
-// output += 'buzz';}
-// console.log (output);
-// }
+for (let i=1; i<100; i++) 
+{
+let output ='';
+if ( i%3===0){
+}
+output +='fizz';
+if ( i%5===0){
+output += 'buzz';}
+console.log (output);
+}
 //Ejercicio 4
 
 ///////////////////////////
@@ -84,3 +86,51 @@ for (let i = 1; i <= 5; i++) {
      star += "\n";
  }
  console.log (star);
+
+//ejercicio fibo "otro"
+ 
+let fibNum0 = 0;
+let fibNum1 = 1;
+let positionFib = 1;
+let i = 2;
+let result = 0;// = fibNum1 + fibNum0; // => fibNum2.
+
+if (positionFib < i) {
+    result = positionFib;
+} else {
+    while (i <= positionFib)
+    {
+      result = fibNum1 + fibNum0;
+      fibNum0 = fibNum1;
+      fibNum1 = result;
+      i++;
+    }
+}
+console.log ('the ' +positionFib+ 'th position of the fibonacci scale is '+ result);
+
+ //ejercicio fibo
+ 
+//  let fibNum0 = 0;
+//  let fibNum1 = 1;
+//  let positionFib = 2;
+//  let i = 1;
+//  let result = fibNum1 + fibNum0; // => fibNum2.
+//  while ((i < positionFib) && (positionFib > 0))
+//  {
+//    result = result + fibNum0;
+//    fibNum0 = fibNum1;
+//    fibNum1 = result;
+//    // console.log (result); 
+//    i++;
+//  }
+//  if (positionFib === 0){
+//    result = fibNum0;
+//  }
+//  console.log ('the ' +positionFib+ 'th position of the fibonacci scale is '+ result);
+//Both excercises have the same variable so one is commented in order to not disrupt the solutions for any of the 2 ways I have resolved the excercise.
+
+
+
+
+
+
