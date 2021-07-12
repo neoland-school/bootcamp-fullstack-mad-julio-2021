1a587866e0bfd3191d669da69d8b70dc50d37bdd
Veronica Tobias <vekatobias@outlook.es>
2021-07-12 14:11:17 +0200

    exercisesjs_function1207

---
+++ VERONICA/exercises_js/class12july.js
@@ -0,0 +1,40 @@
+//Multiplicando con funciones
+
+// function multNumber(numberOne, numbertwo){
+//     return numberOne * numbertwo;
+// }
+
+// console.log(multNumber (5,3));
+
+// function que concatene 'hola' a un string de entrada
+
+// function string (stringWord){
+//     return stringWord + 'Hola';
+// }
+
+// console.log (string('Hello'));
+
+// Function que dado dos boleanos devuelva el AND de estos
+
+// function myBoolean (wordTrue,wordFalse){
+// //     wordTrue = 5; // true
+// //     wordFalse = 11; // false
+//     console.log (wordTrue >= 5 && wordFalse <= 10); // true && false = false
+// }
+// myBoolean(5,11);
+
+// function que devuelva tantos asterisco como indique un parametro de entrada
+
+
+function asterisco (printAlot){
+    print = '*';
+}
+for (let i = 1; i < printAlot; i++){
+       asterisco += '*';
+       return print;
+}
+
+asterisco();
+
+//function que multiplique dos numeros sin usar
+
