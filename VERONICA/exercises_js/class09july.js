1c5d8b4791a8c014b0d558ed9571a0c7261b74cd
Veronica Tobias <vekatobias@outlook.es>
2021-07-12 09:03:45 +0200

    without_comments

--- VERONICA/exercises_js/class09july.js
+++ VERONICA/exercises_js/class09july.js
@@ -1,8 +1,8 @@
 // let sumAcc = 0;
 // // sumAcc += 1;
 // // sumAcc += 2; // SumAcc + sumAcc = 2; Es equivalente
-// // sumAcc += 3; //SumaAcc + 
-// // sumAcc += 4;
+// // sumAcc += 3; //SumAcc + sumAcc = 3;
+// // sumAcc += 4; //
 // // sumAcc += 5;
 // // sumAcc += 6;
 // // sumAcc += 7;
@@ -27,19 +27,19 @@
  * velocidad a la que tengo que ir por la carretera en funcion de la contaminacion
  */
 
-// const ACTUAL_SPEED = 80;
-// const PORCENTAJE = 65 * ACTUAL_SPEED /100 ;
-// const LIMITE = 90;
-// const MUST_REDUCE = PORCENTAJE < LIMITE ; //true
+const ACTUAL_SPEED = 80;
+const PORCENTAJE = 65 * ACTUAL_SPEED /100 ;
+const LIMITE = 90;
+const MUST_REDUCE = PORCENTAJE < LIMITE ; //true
 
-// console.log(MUST_NOT_REDUCE);
+console.log(MUST_NOT_REDUCE);
 
-// if (MUST_REDUCE){
-//     console.log("Debes reducir velocidad");
-// }
-// else{
-//     console.log("Puedes seguir");
-// }
+if (MUST_REDUCE){
+    console.log("Debes reducir velocidad");
+}
+else{
+    console.log("Puedes seguir");
+}
 
 
 // * A traffic light can be 'green', 'flashing amber', 'amber' or 'red'.
@@ -48,30 +48,30 @@
 // * PD: amber you must stop
 // */
 
-// let traffic_light = 'gren';
+let traffic_light = 'gren';
 
-// if (traffic_light === 'green' || traffic_light === 'flashing amber'){
-//     console.log('Can go on');
-// } else if (traffic_light === 'amber' || traffic_light === 'red'){
-//     console.log('Must stop');
-// } else {
-//     console.log('The traffic light is broken');
-// }
+if (traffic_light === 'green' || traffic_light === 'flashing amber'){
+    console.log('Can go on');
+} else if (traffic_light === 'amber' || traffic_light === 'red'){
+    console.log('Must stop');
+} else {
+    console.log('The traffic light is broken');
+}
 
 //////////////////////// Doing with Switch 
 
-// switch (traffic_light){
-//     case 'green':
-//     case 'flashing amber':
-//     console.log ('Can go on');
-//     break;
-//     case 'amber':
-//     case 'red':
-//     console.log('Must stop');
-//     break;
-//     default:
-//     console.log ('The traffic light is broken');
-// }
+switch (traffic_light){
+    case 'green':
+    case 'flashing amber':
+    console.log ('Can go on');
+    break;
+    case 'amber':
+    case 'red':
+    console.log('Must stop');
+    break;
+    default:
+    console.log ('The traffic light is broken');
+}
 
 // * Given a subject result, build an algorithm that prints your result in letters:
 // * SUSPENSA (<5)
@@ -83,83 +83,83 @@
 
 let subjectResult = 8;
 
-// if (subjectResult < 5){
-//     console.log('Suspensa');
-// } else if (5 <= subjectResult && subjectResult < 7){
-//     console.log('Aprobada');
-// } else if (7 <= subjectResult && subjectResult < 9){
-//     console.log ('Notable');
-// } else if (9 <= subjectResult && subjectResult < 10){
-//     console.log ('Sobresaliente');
-// } else{
-//     console.log('Matricula de honor');
-// }
+if (subjectResult < 5){
+    console.log('Suspensa');
+} else if (5 <= subjectResult && subjectResult < 7){
+    console.log('Aprobada');
+} else if (7 <= subjectResult && subjectResult < 9){
+    console.log ('Notable');
+} else if (9 <= subjectResult && subjectResult < 10){
+    console.log ('Sobresaliente');
+} else{
+    console.log('Matricula de honor');
+}
 
 //////////////// Doing with switch
 
-// switch (subjectResult) {
-//     case 1:
-//         console.log('Suspensa');
-//         break;
-//     case 2:
-//         console.log('Suspensa');
-//         break;
-//     case 3:
-//         console.log('Suspensa');
-//         break;
-//     case 4:
-//         console.log('Suspensa');
-//         break;
-//     case 5:
-//         console.log('Aprobada');
-//         break;
-//     case 6:
-//         console.log('Aprobada');
-//         break;
-//     case 7:
-//         console.log('Notable');
-//         break;
-//     case 8:
-//         console.log('Notable');
-//         break;
-//     case 9:
-//         console.log('Sobresaliente');
-//         break;
-//     case 10:
-//         console.log('MATRICULA DE HONOR');
-//         break;
-//     default:
-//         console.log ('You didnt the examen')
-// }
+switch (subjectResult) {
+    case 1:
+        console.log('Suspensa');
+        break;
+    case 2:
+        console.log('Suspensa');
+        break;
+    case 3:
+        console.log('Suspensa');
+        break;
+    case 4:
+        console.log('Suspensa');
+        break;
+    case 5:
+        console.log('Aprobada');
+        break;
+    case 6:
+        console.log('Aprobada');
+        break;
+    case 7:
+        console.log('Notable');
+        break;
+    case 8:
+        console.log('Notable');
+        break;
+    case 9:
+        console.log('Sobresaliente');
+        break;
+    case 10:
+        console.log('MATRICULA DE HONOR');
+        break;
+    default:
+        console.log ('You didnt the examen')
+}
 
 // - Escribir un programa que dado dos números imprima el mayor de ellos (Easy)
 
-// numberOne = 2;
-// numberTwo = 6;
+numberOne = 2;
+numberTwo = 6;
 
-// let MAYOR = numberOne > numberTwo; //TRUE
-// let MENOR = numberOne < numberTwo; //FALSE
+let MAYOR = numberOne > numberTwo; //TRUE
+let MENOR = numberOne < numberTwo; //FALSE
 
-// if (MAYOR >= MENOR)
-// {
-//   console.log("Es el numero mayor");
-// } 
-// else {
-//   console.log("Es el numero menor");
-// }
+if (MAYOR >= MENOR)
+{
+  console.log("Es el numero mayor");
+} 
+else {
+  console.log("Es el numero menor");
+}
 
 // Given a number, loop from 0 to that number printing if it's even or odd
 
-// let justA_number = 15;
+let justA_number = 15;
 
-// for (let i = 1; i <= justA_number; i++){
-//     if (i%2===0){
-//         console.log(i + 'Par');
-//     }
-//     else {
-//         console.log(i +'Impar');
-//     }
-// }
+for (let i = 1; i <= justA_number; i++){
+    if (i%2===0){
+        console.log(i + 'Par');
+    }
+    else {
+        console.log(i +'Impar');
+    }
+}
 
 // Build an algorithm that loops over numbers from 1 to 100 and prints:
 //  *  - 'Fizz' if the current number is multiple of 3
@@ -168,17 +168,17 @@ let subjectResult = 8;
 
 // NO SOLUCIONADO AUN
 
-// for (let i = 1; i <= 100; i++){
-//     if (i%3===0){
-//         console.log(i +='Fizz');
-//     }
-//     if (i%5===0){
-//         console.log(i +='Buzz');
-//     }
-//     if (i%3===0|| i%5===0){
-//         console.log(i +='FizzBuzz');
-//     }
-// }
+for (let i = 1; i <= 100; i++){
+    if (i%3===0){
+        console.log(i +='Fizz');
+    }
+    if (i%5===0){
+        console.log(i +='Buzz');
+    }
+    if (i%3===0|| i%5===0){
+        console.log(i +='FizzBuzz');
+    }
+}
 
 // print the following pattern. It must be printed using loops
 
@@ -192,41 +192,41 @@ let subjectResult = 8;
 // *      **
 // *      *
 
-let oneSign = '*';
+// let oneSign = '*';
 
-for (let i = 0; i < 1; i++) {
-    oneSign += i;
-    console.log('*');
-}
-for (let i = 0; i < 1; i++) {
-    oneSign += i;
-    console.log('**');
-}
-for (let i = 0; i < 1; i++) {
-    oneSign += i;
-    console.log('***');
-}
-for (let i = 0; i < 1; i++) {
-    oneSign += i;
-    console.log('****');
-}
-for (let i = 0; i < 1; i++) {
-    oneSign += i;
-    console.log('*****');
-}
-for (let i = 0; i < 1; i++) {
-    oneSign += i;
-    console.log('****');
-}
-for (let i = 0; i < 1; i++) {
-    oneSign += i;
-    console.log('***');
-}
-for (let i = 0; i < 1; i++) {
-    oneSign += i;
-    console.log('**');
-}
-for (let i = 0; i < 1; i++) {
-    oneSign += i;
-    console.log('*');
-}
+// for (let i = 0; i < 1; i++) {
+//     oneSign += i;
+//     console.log('*');
+// }
+// for (let i = 0; i < 1; i++) {
+//     oneSign += i;
+//     console.log('**');
+// }
+// for (let i = 0; i < 1; i++) {
+//     oneSign += i;
+//     console.log('***');
+// }
+// for (let i = 0; i < 1; i++) {
+//     oneSign += i;
+//     console.log('****');
+// }
+// for (let i = 0; i < 1; i++) {
+//     oneSign += i;
+//     console.log('*****');
+// }
+// for (let i = 0; i < 1; i++) {
+//     oneSign += i;
+//     console.log('****');
+// }
+// for (let i = 0; i < 1; i++) {
+//     oneSign += i;
+//     console.log('***');
+// }
+// for (let i = 0; i < 1; i++) {
+//     oneSign += i;
+//     console.log('**');
+// }
+// for (let i = 0; i < 1; i++) {
+//     oneSign += i;
+//     console.log('*');
+// }
