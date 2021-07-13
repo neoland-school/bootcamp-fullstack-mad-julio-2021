0fe745a90c3626a8ef7e19289d04e8c1aa7bedd6
Veronica Tobias <vekatobias@outlook.es>
2021-07-13 15:11:19 +0200

    exercisesJs_arrays

---
+++ VERONICA/arrays/arraysexercises.js
@@ -0,0 +1,24 @@
+//ARRAY DE NUMEROS PRIMOS
+
+let primo =[2,3,5,7,11,13,17,19,23,29];
+let addition = 0;
+// console.log(primo[2]);
+// console.log(primo[2]*[7]); // Multiplicando la posicion 2 con la 7
+// console.log(primo[9]=primo[1]); // numero de la posicion 9 con el de 1 deberia de dar 3
+
+// 0 + 1 // 1 + 2 // 2 + 3
+
+for (let i = 0; i < primo.length; i++){ //Sumando los caracteres del array
+     addition += primo[i];
+}
+
+console.log(addition);
+
+
+// console.log(primo[1]+[2]+[3]+[4]+[5]+[6]+[7]+[8]+[9]+[10]);
+
+// ARRAY DE NUMEROS PARES
+
+// let par = [2,4,6,8,10,12,14,16,18];
+
+// console.log(par[2]);
