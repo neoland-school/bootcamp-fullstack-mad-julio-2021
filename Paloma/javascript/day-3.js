d1877e0c0d6ca9b381ee4f4c7304ed6fd6250426
PalomaReb <paloma.rebolleda@gmail.com>
2021-07-13 15:37:11 +0200

    Javascript day 3 excercises modified

--- Paloma/javascript/day-3.js
+++ Paloma/javascript/day-3.js
@@ -1,14 +1,14 @@
-// // - Escribir una función que dado un numero, devuelva el número al revés
-// // - Escribir una función que dado un número devuelva si es capicua o no (Hint: usar la función anterior)
-// // - Escribir una función que devuelva el tipo del parámetro de entrada
-// // - Escribir varias funciones que calculen área y perímetro de diferentes figuras geometricas (Rectángulo, Cuadrado, triángulo y Círculo)
-// // - Crear las funciones que simulen una calculadora (Suma, Resta, multiplicaciones, divisiones y porcentajes)
-// //Crear una función que dado una URL (Se representa con un string) me diga o no si pertenece al dominio de github. (Medium)
-// //Crear una función calculadora que reciba como parametros: Dos operandos y la función que se quiera utilizar para calcular. (Hard)
-// //Ejemplo calc(3,4,add) devuelve  7
-// //Crear una funcion que dado un NIF o un NIE te indique si es válido o no. En la página del ministerio se explica como se valida (http://www.interior.gob.es/web/servicios-al-ciudadano/dni/calculo-del-digito-de-control-del-nif-nie) (Hard)
+//  - Escribir una función que dado un numero, devuelva el número al revés
+//  - Escribir una función que dado un número devuelva si es capicua o no (Hint: usar la función anterior)
+//  - Escribir una función que devuelva el tipo del parámetro de entrada
+//  - Escribir varias funciones que calculen área y perímetro de diferentes figuras geometricas (Rectángulo, Cuadrado, triángulo y Círculo)
+//  - Crear las funciones que simulen una calculadora (Suma, Resta, multiplicaciones, divisiones y porcentajes)
+// Crear una función que dado una URL (Se representa con un string) me diga o no si pertenece al dominio de github. (Medium)
+// Crear una función calculadora que reciba como parametros: Dos operandos y la función que se quiera utilizar para calcular. (Hard)
+// Ejemplo calc(3,4,add) devuelve  7
+// Crear una funcion que dado un NIF o un NIE te indique si es válido o no. En la página del ministerio se explica como se valida (http://www.interior.gob.es/web/servicios-al-ciudadano/dni/calculo-del-digito-de-control-del-nif-nie) (Hard)
 
-// //first and second excercise- Escribir una función que dado un numero, devuelva el número al revés
+// first and second excercise- Escribir una función que dado un numero, devuelva el número al revés
 // function reverseNum(number) {
 //     let reverse = 0;
 //     let resto = 0;
@@ -33,14 +33,17 @@
 // }
 // console.log(capicua(656));
 
-// // -3rd Excercise--Escribir una función que devuelva el tipo del parámetro de entrada ==> type of
+
+
+
+//  -3rd Excercise--Escribir una función que devuelva el tipo del parámetro de entrada ==> type of
 
 // function param(parametre) {
 //     return (typeof parametre);
 // }
 // console.log(param(4));
 
-// //4Th Excercise--- Escribir varias funciones que calculen área y perímetro de diferentes figuras geometricas (Rectángulo, Cuadrado, triángulo y Círculo)
+//4Th Excercise--- Escribir varias funciones que calculen área y perímetro de diferentes figuras geometricas (Rectángulo, Cuadrado, triángulo y Círculo)
 
 // function areaTriangle(height, base) {
 //     let area = (height * base) / 2;
@@ -71,7 +74,7 @@
 //     return perimetreRectangle;
 // }
 
-// //5th excercise - Crear las funciones que simulen una calculadora (Suma, Resta, multiplicaciones, divisiones y porcentajes)
+// 5th excercise - Crear las funciones que simulen una calculadora (Suma, Resta, multiplicaciones, divisiones y porcentajes)
 // let a = 0; // not necesary to add numbers for this excercise. 
 // let b = 0;
 // function add(a, b) {
@@ -95,13 +98,22 @@
 
 //6th excercise - Crear una función que dado una URL (Se representa con un string) me diga o no si pertenece al dominio de github. (Medium)
 
-function verification(url) {
-    const search = 'github.com';
-    if (url.indexOf(search) != -1) {
-        return true;
-    }
-    else {
-        return 'You wrote this wrong, thanks';
-    }
-}
-console.log (verification ('https://github.com/PalomaReb/bootcamp-fullstack-mad-julio-2021'));
+// function verification(url) {
+//     const search = 'github.com';
+//     if (url.indexOf(search) != -1) {
+//         return true;
+//     }
+//     else {
+//         return 'You wrote this wrong, thanks';
+//     }
+// }
+// console.log (verification ('https://github.com/PalomaReb/bootcamp-fullstack-mad-julio-2021'));
+
+//Crear una función calculadora que reciba como parametros: Dos operandos y la función que se quiera utilizar para calcular. (Hard)
+//Ejemplo calc(3,4,add) devuelve  7
+
+// function calc(a,b,operation){ //usando las functions de operadores de antes y solo es llamar la function cuando se quiera X operador.
+//     return operation (a,b);
+// }
+
+// console.log (calc(3,4 add));
