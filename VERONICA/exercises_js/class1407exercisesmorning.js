de8c4739032f267a72ac81d61098f0db3bc10b8d
Veronica Tobias <vekatobias@outlook.es>
2021-07-14 15:05:48 +0200

    exercises_arrays_14072021

---
+++ VERONICA/exercises_js/class1407exercisesmorning.js
@@ -0,0 +1,91 @@
+// * 1- Escribir una funcion de comparacion para ordenar un array de numeros de mayor a menor
+
+// let arrNumber = [3,1,6,4,2,5];
+
+// function orderGreater(a,b){
+//     return b - a;
+// }
+// console.log(arrNumber.sort(orderGreater));
+
+
+// // * 2- Escribir una funcion de comparacion para ordenar un array de booleanos poniendo primero los false y luego los true
+
+// let arrBoleano = [true, false, true, false, false]; // true , false , true
+
+// function compareBol (a,b){
+//         return a - b;
+   
+// }
+// console.log(arrBoleano.sort(compareBol));
+
+// * 3- Escribir una funcion de comparacion para ordenar un array de strings por su longitud de caracteres de menor a mayor
+
+// let arrStr = ['Hello','bye', 'good', 'bad'];
+
+// function orderByLenght (a,b){
+//    return a.length - b.length;
+// }
+
+// console.log(arrStr.sort(orderByLenght));
+
+// * 4- Escribir una funcion para filtrar un array de numeros dejando solo los numeros pares
+
+// let othersNumbers = [3, 2, 5, 7, 4, 9, 10, 6];
+
+// function onlyNumbersPairs (array){
+//     if (array % 2 === 0){
+//         return array;
+//     }
+// }
+
+// console.log(othersNumbers.filter(onlyNumbersPairs));
+
+
+// * 5- Escribir una funcion para filtrar un array de numeros dejando solo los mayores de 30
+
+// let numberSmallerThanThirty = [30,50,15,33,10,22];
+
+// function smaller (array){
+//     if (array <= 30){
+//         return array;
+//     }
+// }
+
+// console.log(numberSmallerThanThirty.filter(smaller));
+
+// * 6- Escribir una funcion para filtrar un array de booleanos dejando solo los valores true
+
+// crear funcion
+// filtrar un array de boleanos
+// dejar solo los true
+
+
+// let arrBool = [false, true, false, true, false];
+
+// function justTrueBool (array){
+//      if (array === true){
+//          return array;
+//      }
+    
+// }
+
+// console.log(arrBool.filter(justTrueBool));
+
+
+// * 7- Escribir una funcion para filtrar un array de strings dejando los que su longitud sea impar
+
+// crear 1 funcion
+// que filtre un array de String
+// devuelva solo los impar
+
+// let arrStr = ['Nice', 'Beautiful', 'Ugly', 'Fabulous', 'Magic', 'Lives'];
+
+// function justLenghtImpair (arr){
+//      for (let i=0; i<arr.length; i++){
+//          if (arr.length %2!==0){
+//             return arr.length;
+//          }
+//      }
+// }
+
+// console.log(arrStr.filter(justLenghtImpair));
