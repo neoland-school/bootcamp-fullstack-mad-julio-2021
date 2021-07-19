ff1cedc9b2d4ffcbfb36055476541a1183444394
Veronica Tobias <vekatobias@outlook.es>
2021-07-20 00:35:08 +0200

    excercises_dom

--- VERONICA/dom_exercises2/index.js
+++ VERONICA/dom_exercises2/index.js
@@ -1,72 +1,89 @@
 let ciudad = {
-    nombre: 'Santa Marta',
-    descripcion: 'Fue fundada el 29 de julio de 1525 por el español Rodrigo de Bastidas, lo que según los textos, la hace la ciudad en pie más antigua de Colombia. Es conocida por sus actividades turisticas, la historia de sus calles y sus playas.',
+    nombre: 'What Happened in Santa Marta?',
+    descripcion: 'Es conocida por sus actividades turisticas, la historia de sus calles y sus playas.',
     pais: 'Colombia',
     urlImagen: 'https://blog.redbus.co/wp-content/uploads/2017/11/experiencia-santa-marta-colombia-camila-c351f3c56e46da3c095796608021365d.jpg',
     coordenadas: {
         latitud: '11.2407900',
         longitud: '-74.1990400',
     },
-    urlLink: 'https://es.wikipedia.org/wiki/Santa_Marta_(Colombia)'
+    urlLink: 'https://es.wikipedia.org/wiki/Santa_Marta_(Colombia)',
+    day: '19 July 2021'
 }
 
 let ciudadTwo = {
-    nombre: 'Medellin',
-    descripcion: 'Es la ciudad más poblada del departamento y la segunda más poblada del país después de Bogotá.9​ Se asienta en la parte más ancha de la región natural conocida como Valle de Aburrá, en la cordillera central de los Andes.',
+    nombre: 'What Happened in Medellin?',
+    descripcion: 'Es la ciudad más poblada del departamento y la segunda más poblada del país después de Bogotá.',
     pais: 'Colombia',
     urlImagen: 'https://cdn.getyourguide.com/img/location/5cced3e295e02.jpeg/88.jpg',
     coordenadas: {
         latitud: '6.217',
         longitud: '-75.567',
     },
-    urlLink: 'https://es.wikipedia.org/wiki/Medell%C3%ADn'
+    urlLink: 'https://es.wikipedia.org/wiki/Medell%C3%ADn',
+    day: '19 July 2021'
+
 }
 
 let ciudadThree = {
-    nombre: 'Cartagena',
-    descripcion: 'Desde 1991 Cartagena es un Distrito Turístico y Cultural.8​ La ciudad está localizada a orillas del mar Caribe.',
+    nombre: 'What Happened in Cartagena?',
+    descripcion: 'Cartagena tiene uno de los puertos de mayor importancia de Colombia, del Caribe y del mundo, así como un célebre destino turístico.',
     pais: 'Colombia',
-    urlImagen: 'https://upload.wikimedia.org/wikipedia/commons/e/e8/Baluarte_de_Santiago_CTG_11_2019_9804.jpg',
+    urlImagen: 'https://conocedores.com/wp-content/uploads/2019/07/cartagena-26072019.jpg',
     coordenadas: {
         latitud: '10.39972',
         longitud: '-75.51444',
     },
-    urlLink: 'https://es.wikipedia.org/wiki/Cartagena_de_Indias'
+    urlLink: 'https://es.wikipedia.org/wiki/Cartagena_de_Indias',
+    day: '19 July 2021'
+
 }
 
 
 
 function nameCities(ciudad) {
-
+    //CREAR ELEMENTOS
     const containerFather = document.createElement('div');
     const containerChildOne = document.createElement('div')
     const containerChildTwo = document.createElement('div')
-    containerFather.appendChild(containerChildOne);
-    containerFather.appendChild(containerChildTwo);
+    const containerChildThree = document.createElement('div')
     const imagen = document.createElement('img');
     const title = document.createElement('h1');
     const paragraphDescription = document.createElement('p');
     const link = document.createElement('a');
-
+    const day = document.createElement('p');
 
     // Styles
     containerFather.classList.add('card');
     containerChildTwo.classList.add('text-container');
+    title.classList.add('title');
     imagen.classList.add('img-foto');
+    day.classList.add('text-day');
+    paragraphDescription.classList.add('descript-card')
+    containerChildThree.classList.add('div-link');
+    link.classList.add('link-info');
+    
+   
 
-    // Content with the object
+    // ENLAZANDO CON EL OBJETO
     imagen.src = ciudad.urlImagen;
     title.textContent = ciudad.nombre;
     paragraphDescription.textContent = ciudad.descripcion;
-    link.textContent = 'Para más información.';
+    link.textContent = 'For more information.';
     link.href = ciudad.urlLink;
+    day.textContent = ciudad.day;
 
 
-
+    //AGREGANDOLO AL HTML
+    containerFather.appendChild(containerChildOne);
+    containerFather.appendChild(containerChildTwo);
+    containerFather.appendChild(containerChildThree);
     containerChildOne.appendChild(imagen);
+    containerChildTwo.appendChild(day);
     containerChildTwo.appendChild(title);
     containerChildTwo.appendChild(paragraphDescription);
-    containerChildTwo.appendChild(link);
+    containerChildThree.appendChild(link);
+  
 
     document.body.appendChild(containerFather);
 
