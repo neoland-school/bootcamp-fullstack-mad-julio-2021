6b8bb9829fb0d08dd25118ac2cd887be1e29a7e7
Veronica Tobias <vekatobias@outlook.es>
2021-08-01 15:17:13 +0200

    cambios_calculadora

---
+++ VERONICA/REACTJS/exercise_contador/contador_react/src/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="main-container">
+      <h1>Contador</h1>
+      <p></p>
+    </div>
+  );
+}
+
+export default App;
